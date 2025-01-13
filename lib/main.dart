998e1d71416eefe0375fd6d151935da3714339de
Indra090800 <indramaulana.9c@gmail.com>
2025-01-13 14:22:38 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
-import 'package:provider/provider.dart';
 import 'package:xhalona_pos/core/theme/theme.dart';
-import 'package:xhalona_pos/views/authentication/login/login_provider.dart';
-import 'package:xhalona_pos/views/home/fragment/pos/pos_controller.dart';
-import 'package:xhalona_pos/views/home/home_provider.dart';
 import 'package:xhalona_pos/views/launcher/splash_screen.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,18 +14,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Builder(
-        builder: (context) {
-          ResponsiveScreen.init(context, designSize: const Size(390, 844));
-          return MaterialApp(
-            theme: ThemeData(
-              fontFamily: 'GoogleSans',
-            ),
-            debugShowCheckedModeBanner: false,
-            builder: FlutterSmartDialog.init(),
-            home: SplashScreen(),
-          );
+      builder: (context) {
+        ResponsiveScreen.init(context, designSize: const Size(390, 844));
+        return MaterialApp(
+          theme: ThemeData(
+            fontFamily: 'GoogleSans',
+          ),
+          debugShowCheckedModeBanner: false,
+          builder: FlutterSmartDialog.init(),
+          home: SplashScreen(),
+        );
       },
     );
   }
 }
-
