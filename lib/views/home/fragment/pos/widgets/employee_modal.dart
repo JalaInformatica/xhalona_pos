6ff3f4cbca09662c9403bb35f54046c4e948e0c2
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2025-01-15 17:55:29 +0700

    employee modal

---
+++ lib/views/home/fragment/pos/widgets/employee_modal.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:xhalona_pos/core/theme/theme.dart';
+import 'package:xhalona_pos/models/dao/employee.dart';
+import 'package:xhalona_pos/views/home/fragment/pos/widgets/employee_modal_controller.dart';
+import 'package:xhalona_pos/widgets/app_normal_button.dart';
+import 'package:xhalona_pos/widgets/app_text_field.dart';
+
+// ignore: must_be_immutable
+class EmployeeModal extends StatelessWidget {
+  final EmployeeModalController controller = Get.put(EmployeeModalController());
+  Timer? _debounce;
+  Function(EmployeeDAO) onEmployeeSelected;
+
+  EmployeeModal({
+    super.key, 
+    required this.onEmployeeSelected
+  });
+
+  void _onChanged(String query) {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(seconds: 1), () {
+      controller.fetchEmployees(filter: query);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(()=> Column(
+      spacing: 5.h,
+      children: [
+        AppTextField(
+          context: context,
+          hintText: "Cari Terapis",
+          onChanged: _onChanged,
+          autofocus: true,
+        ),
+        Expanded(child: 
+          ListView.separated(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            itemCount: controller.employees.length,
+            itemBuilder: (context, index) {
+              EmployeeDAO employee = controller.employees[index];
+              return AppTextButton(
+                foregroundColor: AppColor.blackColor,
+                onPressed: () {
+                  onEmployeeSelected(employee);
+                },
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  employee.fullName,
+                  style: AppTextStyle.textBodyStyle(),
+                ),
+                shape: BeveledRectangleBorder(
+                  side: BorderSide(color: AppColor.grey100)
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => SizedBox(height: 5.h,),
+          )
+        ),
+      ],
+    ));
+  }
+  
+}
