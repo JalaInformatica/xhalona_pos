514e75d490a7e213e44d941a61a4db97e48731b7
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2025-01-15 17:57:26 +0700

    pos and home

--- lib/views/home/fragment/pos/pos_controller.dart
+++ lib/views/home/fragment/pos/pos_controller.dart
@@ -1,10 +1,43 @@
+import 'dart:async';
+
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:get/get.dart';
+import 'package:xhalona_pos/models/dao/employee.dart';
 import 'package:xhalona_pos/models/dao/product.dart';
+import 'package:xhalona_pos/models/dao/transaction.dart';
+import 'package:xhalona_pos/models/dto/transaction.dart';
 import 'package:xhalona_pos/repositories/product/product_repository.dart';
+import 'package:xhalona_pos/repositories/employee/employee_repository.dart';
+import 'package:xhalona_pos/repositories/transaction/transaction_repository.dart';
 
 class PosController extends GetxController {
+  final TransactionRepository _transactionRepository = TransactionRepository();
+  final ProductRepository _productRepository = ProductRepository();
+
   var products = <ProductDAO>[].obs;
   var isLoading = true.obs;
+
+  var isOpenTransaksi = false.obs;
+
+  var productFilterValue = "".obs;
+
+  var currentTransactionId = "".obs;
+
+  var currentTransaction = <TransactionHeaderDAO>[].obs;
+  var currentTransactionDetail = <TransactionDetailDAO>[].obs;
+  var selectedProductPartIdToTrx = "".obs;
+  var isAddingProductToTrx= false.obs;
+  
+  Timer? _debounce;
+
+  void updateProductFilterValue(String newFilterValue) {
+    if (_debounce?.isActive ?? false) _debounce!.cancel(); 
+
+    _debounce = Timer(const Duration(seconds: 1), () {
+      productFilterValue.value = newFilterValue; 
+      fetchProducts(); 
+    });
+  }
 
   @override
   void onInit() {
@@ -15,13 +48,45 @@ class PosController extends GetxController {
   Future<void> fetchProducts() async {
     try {
       isLoading.value = true;
-      final result = await ProductRepository().getProducts(
+      final result = await _productRepository.getProducts(
         pageRow: 24,
         isActive: 1,
+        filterValue: productFilterValue.value
       );
       products.value = result;
     } finally {
       isLoading.value = false;
     }
   }
+
+  Future<void> addProductToTrx(ProductDAO product) async {
+    selectedProductPartIdToTrx.value = product.partId;
+    isAddingProductToTrx.value = true;
+    if(currentTransactionId.value == ""){
+      currentTransactionId.value = await _transactionRepository.addTransactionHeader(
+        TransactionDTO()
+      );
+    }
+    await _transactionRepository.addTransactionDetail(
+        [
+          TransactionDetailDTO(
+          salesId: /*'AM0000001443'*/ currentTransactionId.value,
+          partId: product.partId,
+          qty: 1,
+          price: product.unitPriceNet
+        )]
+    );
+    currentTransaction.value = await _transactionRepository.getTransactionHeader(transactionId: currentTransactionId.value);
+    currentTransactionDetail.value = await _transactionRepository.getTransactionDetail(transactionId: currentTransactionId.value);
+    isAddingProductToTrx.value = false;
+    selectedProductPartIdToTrx.value = "";
+  }
+
+  // Future<List<EmployeeDAO>> fetchEmployees({required String filter}) async{
+  //   return await _employeeRepository.getEmployees(filterField: filter);
+  // }
+
+  // Future<List<EmployeeDAO>> getEmployees(String filter, [LoadProps? props]) async{
+  //   return await _employeeRepository.getEmployees(filterField: filter);
+  // }
 }
--- lib/views/home/home_controller.dart
+++ lib/views/home/home_controller.dart
@@ -1,23 +1,17 @@
-import 'package:dropdown_search/dropdown_search.dart';
 import 'package:get/get.dart';
-import 'package:xhalona_pos/models/dao/employee.dart';
 import 'package:xhalona_pos/models/dao/structure.dart';
 import 'package:xhalona_pos/models/dao/user.dart';
-import 'package:xhalona_pos/repositories/employee/employee_repository.dart';
 import 'package:xhalona_pos/repositories/structure/structure_repository.dart';
 import 'package:xhalona_pos/repositories/user/user_repository.dart';
 
 class HomeController extends GetxController {
   final StructureRepository _structureRepository = StructureRepository();
   final UserRepository _userRepository = UserRepository();
-  final EmployeeRepository _employeeRepository = EmployeeRepository();
-
+  
   var isMenuLoading = true.obs;
   var menuData = <MenuDAO>[].obs;
   var profileData = UserDAO().obs;
   var selectedMenuName = "pos".obs;
-
-  var isOpenTransaksi = false.obs;
 
   @override
   void onInit() {
@@ -31,7 +25,5 @@ class HomeController extends GetxController {
     isMenuLoading.value = false;
   }
 
-  Future<List<EmployeeDAO>> getEmployees(String filter, [LoadProps? props])async{
-    return await _employeeRepository.getEmployees(filterField: filter);
-  }
+ 
 }
