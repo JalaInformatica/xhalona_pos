3511790f09577818f4007f39e23904448f27eb73
Ichlas Wardy Gustama <cahyo4713@gmail.com>
2025-02-20 12:24:51 +0700

    chore(fix): restore '@semantic-release/git' plugin and clean up version update command in release config

--- release.config.js
+++ release.config.js
@@ -3,10 +3,10 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/git',
     {
       path: '@semantic-release/exec',
-      cmd: 'sed -i "s/version: .*/version: ${nextRelease.version}/" pubspec.yaml || echo "Failed to update version in pubspec.yaml"',
+      cmd: 'sed -i "s/version: .*/version: ${nextRelease.version}/" pubspec.yaml',
     },
+    '@semantic-release/git',
   ],
 };
